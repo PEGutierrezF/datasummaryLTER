1f523a2f35dfb89205004d2aa4f23f489d223229
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-03-19 15:38:51 -0600

    data

---
+++ shrimps.R
@@ -0,0 +1,28 @@
+
+
+shrimps <- read.csv("data/shrimp.csv")
+head(shrimps)
+
+
+sh1 <- shrimps %>%
+  group_by(stream, year, Month) %>% 
+  summarise(mean = mean(Abundance, na.rm=T),
+            sd = sd(Abundance, na.rm = T)) %>%
+  arrange(year, match(Month, month.name))
+sh1
+
+
+sh.a <- filter(sh1, stream=="QPB")  %>% rowid_to_column(var='observation')
+sh.b <- filter(sh1, stream=="QPA")  %>% rowid_to_column(var='observation')
+
+
+qpa <- ggplot(sh.a, aes(x=observation , y=mean)) + 
+  geom_line() +
+  geom_point() 
+
+qpb <- ggplot(sh.b, aes(x=observation , y=mean)) + 
+  geom_line() +
+  geom_point() 
+
+qpa/qpb
+
