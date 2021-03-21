8792fdcbf595ef5063245c7ed829323b7dd887bd
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-03-20 20:31:15 -0600

    date

---
+++ libraries.R
@@ -0,0 +1,6 @@
+
+
+install.packages("xlsx")
+
+libraries <- c( "ggplot2", "plyr","dplyr", 'patchwork','tidyverse')
+lapply(libraries, require, character.only = TRUE)
--- shrimps.R
+++ shrimps.R
@@ -4,23 +4,28 @@ shrimps <- read.csv("data/shrimp.csv")
 head(shrimps)
 
 sh1 <- shrimps %>%
-  group_by(stream, year, Month) %>% 
+  group_by(stream, year, Month,day) %>% 
   summarise(mean = mean(Abundance, na.rm=T),
             sd = sd(Abundance, na.rm = T)) %>%
   arrange(year, match(Month, month.name))
 sh1
 
-#df$Year_Month <- paste0(df$Month, " ", df$Year)
 
-sh.a <- filter(sh1, stream=="QPB")  %>% rowid_to_column(var='observation') 
+
+
+sh.a <- filter(sh1, stream=="QPB")  %>% rowid_to_column(var='observation')
+sh.a$date <-as.Date(with(sh.a,paste(year,Month,day,sep="-")),"%Y-%b-%d")
+
+
 sh.b <- filter(sh1, stream=="QPA")  %>% rowid_to_column(var='observation')
+sh.b$date <-as.Date(with(sh.b,paste(year,Month,day,sep="-")),"%Y-%b-%d")
 
-qpa <- ggplot(sh.a, aes(x=observation , y=mean)) + 
+qpa <- ggplot(sh.a, aes(x=date , y=mean)) + 
   geom_line() +
   geom_point()+
   theme_classic()
 
-qpb <- ggplot(sh.b, aes(x=observation , y=mean)) + 
+qpb <- ggplot(sh.b, aes(x=date , y=mean)) + 
   geom_line() +
   geom_point()+
   theme_classic()
