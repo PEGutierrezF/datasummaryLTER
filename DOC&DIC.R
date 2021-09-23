207d54db77f0eff8e7995cd20916f446fc774dbf
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-09-23 17:28:13 -0600

    data

--- DOC&DIC.R
+++ DOC&DIC.R
@@ -18,19 +18,32 @@ head(Carbon)
 Carbon$date <-as.POSIXct(Carbon$date,"%Y-%m-%d",tz = "UTC")
 
 ggplot(Carbon, aes(x=date, y=value, color =stream)) +
-  geom_line() +
-  facet_grid(~variable)
+  geom_line(size=1.2) +
+  facet_grid(~variable) +
+  theme(strip.text = element_text(face="bold", size=16),
+        strip.background = element_rect(fill="white", colour="black",size=1))
        
-
-Carbon %>% group_by(stream) %>% summarise(Mean_sales = mean(value))
 
 
 Carbon %>%
   dplyr::group_by(stream, variable) %>%
   dplyr::summarize(mean=mean(value, na.rm=TRUE), 
-#  low = CI90lo(value), 
- # high= CI90hi(value),
   min=min(value, na.rm=TRUE),
   max=max(value,na.rm=TRUE), 
   sd= sd(value, na.rm=TRUE))
+
+
+a <- Carbon[c(1:118, 331:434), ]
+a
+
+a %>%
+  dplyr::group_by(stream, variable) %>%
+  dplyr::summarize(mean=mean(value, na.rm=TRUE), 
+                   min=min(value, na.rm=TRUE),
+                   max=max(value,na.rm=TRUE), 
+                   sd= sd(value, na.rm=TRUE))
+
+ggplot(a, aes(x=date, y=value, color =stream)) +
+  geom_line(size=1.2) +
+  ylim(0,6)
 
