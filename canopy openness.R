6f38644d068ecbfd7e15dcd9711f580cea6491a4
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-04-03 19:54:01 -0400

    data

--- canopy openness.R
+++ canopy openness.R
@@ -16,40 +16,37 @@ canopyop$date <-as.POSIXct(canopyop$date,"%Y-%m-%d",tz = "UTC")
 
 canopyop$sd = as.numeric(canopyop$sd)
 
-c <- ggplot(canopyop, aes(x=date,y=value, colour=stream)) +
+
+figure.canopy <- ggplot(canopyop, aes(x=date,y=value, colour=stream)) +
   geom_line(size=0.8) + 
-  scale_color_manual(values=c('#ce1256','#0570b0'))+
+  scale_color_manual(name = "Streams" ,
+                     labels = c("Priea A", "Prieta B"),
+                     values=c('#ce1256','#0570b0'))+
   geom_point() +
   geom_errorbar(aes(ymax=value+sd, ymin=value-sd), na.rm=TRUE, 
                 #position = position_dodge(width = 0.9),stat = "identity", 
                 width = 0, colour = "gray50") +
   # Labels
-  labs(x= '', y= 'Canopy openness (%)') +
- # labs(tag = "B") +
+  labs(x= 'Year', y= 'Canopy openness (%)') +
+  # labs(tag = "B") +
   
   theme_bw()+
-  theme(legend.position="none")  +
+  
+  #Legend 
+  theme(legend.key.size = unit(0.6, "cm"))+
+  theme(legend.title=element_text(size=14)) + # legend title size
+  theme(legend.text = element_text(color = "black", size = 16))+  #factor name 
+  theme(legend.position=c(0.85, 0.9)) +
+  #theme(legend.title=element_blank()) +
+  theme(legend.text=element_text(size=14)) +  
+
   ylim(0,100)+
   #Axis  
-  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
-  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
-  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
-  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black"))  #subaxis y
-c
+  theme(axis.title.x = element_text(size = 16, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 16, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black"))  #subaxis y
 
-c1 <- c + annotate("rect", xmin = as.POSIXct("2017-09-6"), xmax = as.POSIXct("2017-09-21"), 
-                   ymin = -Inf, ymax = Inf,  fill = "#df65b0", alpha=.5) +
-  
-  geom_segment(aes(x = as.POSIXct(c("2017-02-01")), y = 77, xend = as.POSIXct(c("2017-02-01")), yend = 69), 
-               arrow = arrow(length = unit(0.2, "cm")),size = 1, color = "black") +
-  
-  geom_segment(aes(x = as.POSIXct(c("2017-11-30")), y = 77, xend = as.POSIXct(c("2017-11-30")), yend = 69), 
-               arrow = arrow(length = unit(0.2, "cm")),size = 1, color = "black") +
-  
-  geom_segment(aes(x = as.POSIXct(c("2018-06-01")), y = 77, xend = as.POSIXct(c("2018-06-01")), yend = 69), 
-               arrow = arrow(length = unit(0.2, "cm")),size = 1, color = "black") +
-  
-  geom_segment(aes(x = as.POSIXct(c("2019-02-01")), y = 77, xend = as.POSIXct(c("2019-02-01")), yend = 69), 
-               arrow = arrow(length = unit(0.2, "cm")),size = 1, color = "black")
+figure.canopy
 
-c1
+ggsave("Canopy openness.jpeg", figure.canopy, width = 200, height = 180, units = "mm")
