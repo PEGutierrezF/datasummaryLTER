678f60e88493c87b9a1c6b1fd4ef3408b50d2309
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-04-03 22:24:37 -0400

    data

---
+++ Leaflitter fall.R
@@ -0,0 +1,58 @@
+
+
+
+
+# ---------------------------------------------
+# Leaf litter fall
+# 03 Apr 2022
+# Pablo E. Gutiérrez-Fonseca
+# pabloe.gutierrezfonseca@gmail.com
+# ---------------------------------------------
+#  
+
+
+
+leaf <- "D:/LTER/24 data summary/datasummaryLTER/data/all_variables.xlsx"
+excel_sheets(path = leaf)
+leaflitter <- read_excel(path = leaf, sheet = "leaf")
+
+leaflitter$date <-as.POSIXct(leaflitter$date,"%Y-%m-%d",tz = "UTC")
+leaflitter$sd = as.numeric(leaflitter$sd)
+
+
+figure.litter <- ggplot(leaflitter, aes(x=date,y=value, colour=stream)) +
+  geom_line(size=0.8) + 
+  scale_color_manual(name = "Streams" ,
+                     labels = c("Priea A", "Prieta B"),
+                     values=c('#ce1256','#0570b0')) +
+  
+  # Number of digits
+  scale_y_continuous(labels = scales::number_format(accuracy = 1),
+                     breaks = seq(0, 13, by=3), limits=c(0,13)) +
+  
+  #Labels
+  labs(x= 'Year', y= "Litterfall input rate ("*g~m^-2~d^-1*")") + #  ("*g~m^-2~d^-1*")
+
+  
+  geom_point() +
+  geom_errorbar(aes(ymax=value+sd, ymin=value-sd),na.rm=TRUE, 
+                # position = position_dodge(width = 0.9),stat = "identity", 
+                width = 0, colour = "gray50") +
+  theme_bw() +
+
+  #Legend
+  theme(legend.key.size = unit(0.6, "cm"))+
+  theme(legend.title=element_text(size=14)) + # legend title size
+  theme(legend.text = element_text(color = "black", size = 16))+  #factor name 
+  theme(legend.position=c(0.85, 0.9)) +
+  #theme(legend.title=element_blank()) +
+  theme(legend.text=element_text(size=14)) +  
+  
+  theme(axis.title.x = element_text(size = 16, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 16, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black"))  #subaxis y
+
+figure.litter
+
+ggsave("Litter fall.jpeg", path = "figures", figure.litter, width = 200, height = 180, units = "mm")
---
+++ Shrimp abundance.R
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+shrimp <- "D:/LTER/24 data summary/datasummaryLTER/data/all_variables.xlsx"
+excel_sheets(path = shrimp)
+data.shrimp <- read_excel(path = shrimp, sheet = "shrimp")
+
+data.shrimp$date <-as.POSIXct(data.shrimp$date,"%Y-%m-%d",tz = "UTC")
+data.shrimp$sd = as.numeric(data.shrimp$sd)
+
+
+
+figure.shrimp <- ggplot(data.shrimp, aes(x=date,y=value, colour=stream)) +
+  geom_line(size=0.8) + 
+  scale_color_manual(name = "Streams" ,
+                     labels = c("Priea A", "Prieta B"),
+                     values=c('#ce1256','#0570b0')) +
+  
+  # Number of digits
+  scale_y_continuous(labels = scales::number_format(accuracy = 1),
+                     breaks = seq(0, 20, by=3), limits=c(0, 20)) +
+  
+  #Labels
+  labs(x= 'Year', y= "Decapoda abundance (" *indv~m^-2* ")") + #  ("*g~m^-2~d^-1*")
+  
+  
+  geom_point() +
+#  geom_errorbar(aes(ymax=value+sd, ymin=value-sd),na.rm=TRUE, 
+                # position = position_dodge(width = 0.9),stat = "identity", 
+#                width = 0, colour = "gray50") +
+  theme_bw() +
+  
+  #Legend
+  theme(legend.key.size = unit(0.6, "cm"))+
+  theme(legend.title=element_text(size=14)) + # legend title size
+  theme(legend.text = element_text(color = "black", size = 16))+  #factor name 
+  theme(legend.position=c(0.85, 0.9)) +
+  #theme(legend.title=element_blank()) +
+  theme(legend.text=element_text(size=14)) +  
+  
+  theme(axis.title.x = element_text(size = 16, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 16, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black"))  #subaxis y
+
+figure.shrimp
+
+ggsave("Decapoda.jpeg", path = "figures", figure.litter, width = 160, height = 180, units = "mm")
