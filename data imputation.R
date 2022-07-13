c1e07f94607d4a8384a763f6d982fcb9da9eb2bd
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-07-12 20:21:40 -0400

    data

---
+++ data imputation.R
@@ -0,0 +1,20 @@
+
+
+
+
+# ---------------------------------------------
+# Data imputation:  Stable isotopes
+# 12 Jul 2022
+# Pablo E. Gutiérrez-Fonseca
+# pabloe.gutierrezfonseca@gmail.com
+# ---------------------------------------------
+#  
+
+
+sourcesQPA <- read.csv("data/sourcesQPA.csv")
+head(sourcesQPA)
+
+
+sourcesQPA_imp <- mice(sourcesQPA, m = 100)
+
+
