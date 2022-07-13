32d2d461bd4ad7e33e75cf23229342fa0ce3d6d7
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-07-13 13:20:24 -0400

    data

--- data imputation.R
+++ data imputation.R
@@ -22,9 +22,16 @@ excel_sheets(path = network.data)
 
 sourcesQPA <- read_excel(path = network.data, sheet = "sourcesQPA")
 
-sourcesQPA_new <- sourcesQPA %>% select(-month)
+# Remove characters columns 
+sourcesQPA_new <- sourcesQPA %>% select(-date, -month)
+# check class
+sapply(sourcesQPA_new, class)
+# add NA to empty cells
+sourcesQPA_new[sourcesQPA_new == ""] <- NA
+# convert in data.frame
+sourcesQPA_new <- as.data.frame(sourcesQPA_new)
 
-set.seed(14)
+set.seed(14) # start in the same place, same results
 sourcesQPA_imp <- missForest(sourcesQPA_new)
 
 #check imputed values
--- libraries.R
+++ libraries.R
@@ -10,7 +10,7 @@
 # ---------------------------------------------
 #  
 
-install.packages("mice")
+# install.packages("mice")
 
 libraries <- c( "ggplot2", "plyr","dplyr", 'patchwork','tidyverse',
                 "readxl", "missForest", "readxl")
