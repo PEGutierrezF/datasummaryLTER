9022cfc4b02105f94a3009353e0b50cf7fed47f8
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-07-30 13:08:05 -0400

    data

--- libraries.R
+++ libraries.R
@@ -10,9 +10,16 @@
 # ---------------------------------------------
 #  
 
-install.packages("xlsx")
+
+
+# cleans global environment
+rm(list = ls())
+
+# install.packages("xlsx")
 
 libraries <- c( "ggplot2", "plyr","dplyr", 'patchwork','tidyverse',
                 "readxl", "missForest", "readxl",'xlsx')
 
 lapply(libraries, require, character.only = TRUE)
+
+
